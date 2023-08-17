0dcb599cf793e5778582beac6b40d58cf65f8a45
datax-snowflake <142255470+datax-snowflake@users.noreply.github.com>
2023-08-17 15:12:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,15 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
+
 
 #API Session
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#streamlit.text(fruityvice_response.json())
+
+# json normalize
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# dataframe of normalized json
+streamlit.dataframe(fruityvice_normalized)
