416293b9a66e6ca6627ccaf6fba32e3e93721cf5
datax-snowflake <142255470+datax-snowflake@users.noreply.github.com>
2023-08-17 22:00:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ my_cur = my_cnx.cursor()
 #my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
 
-my_cur.execute("SELECT * FROM fruit_load_list )
+my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains")
 streamlit.text(my_data_rows)
