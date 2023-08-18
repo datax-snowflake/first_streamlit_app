657be6e2f8c976900a8727e3ed7aa479f04a2e09
datax-snowflake <142255470+datax-snowflake@users.noreply.github.com>
2023-08-17 22:09:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ streamlit.dataframe(fruits_to_show)
 
 #API Session
 streamlit.header("Fruityvice Fruit Advice!")
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
@@ -50,3 +49,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains")
 streamlit.text(my_data_rows)
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add','TYPE HERE')
+streamlit.write('Thaks to add', add_my_fruit)
